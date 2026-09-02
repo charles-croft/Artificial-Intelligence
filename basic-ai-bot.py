52ed6806cd45c8a5c9d0331d8f213d1f068da818
charles-croft <chuckiecrc1@outlook.com>
2026-09-02 17:39:57 +1000

    Enhance AI Chatbot Project

--- basic-ai-bot.py
+++ basic-ai-bot.py
@@ -6,7 +6,32 @@ mood = input().lower()
 if mood == "good":
     print("That's great. I'm feeling well today too!")
 elif mood == "bad":
-    print("Hope you get better later")
+    print("Hope you get better later.")
 else:
     print("Understood.")
-print(f"Well it was great chatting with you. Until next time {name}!")
+print("Do you like sports? (yes/no): ")
+sports = input().lower()
+if sports == "yes":
+    print("It's good to get active!")
+if sports == "no":
+    print("Reasonable choice. Some sports can be quite extreme.")
+else:
+    print("Good choice.")
+print("Do you enjoy academics? (yes/no): ")
+academics = input().lower()
+if academics == "yes":
+    print("I'm glad you do!")
+if academics == "no":
+    print("Academic subjects such as mathematics can be extremely tricky.")
+else:
+    print("I understand your logic.")
+if sports == "yes":
+    if academics == "yes":
+        print(f"Keep practicing your sports and academics {name}!")
+    else:
+        print(f"Keep practicing you sports {name}!")
+else:
+    if academics == "yes":
+        print(f"Keep practicing your academics {name}!")
+    else:
+        print(f"Keep practicing your hobbies {name}!")
